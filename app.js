4c39f15ac335dd727fe7b572a7ddd3b81f28c449
yuvraj-05828 <yuvraj05828@gmail.com>
2023-12-19 12:23:52 +0530

    update 3

--- app.js
+++ app.js
@@ -1,138 +1,140 @@
-let randomMealCard = document.getElementById("random-card");
-let homeButton = document.getElementById("home");
-let searchBar = document.getElementById("search");
-let searchButton = document.getElementById("search-btn");
-let searchInput = document.getElementById("input");
+let mealCard = document.getElementById("random-card");
+let homeBtn = document.getElementById("home");
+let searchInput = document.getElementById("search");
+let searchBtn = document.getElementById("search-btn");
+let userInput = document.getElementById("input");
 let modal = document.getElementById("modal-1");
-let ingredients = document.getElementById("information");
-let span = document.getElementsByClassName("exit")[0];
-let mode = document.getElementsByClassName("category");
-let results = document.getElementById("output");
+let ingredientList = document.getElementById("information");
+let exitSpan = document.getElementsByClassName("exit")[0];
+let categories = document.getElementsByClassName("category");
+let resultOutput = document.getElementById("output");
+let mealCards;
+let selectedCategory;
 
-let mealCard;
-let selectedMode;
 
-async function randomMeal() {
-  let arr;
-
+async function fetchRandomMeal() {
+  let mealData;
   await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
     .then((data) => data.json())
     .then((e) => {
-      arr = e;
+      mealData = e;
+      // console.log(mealData)
     })
     .catch((error) => console.log(error));
-
-  randomMealCard.innerHTML = `<div class="meal">
-    <img class="meals" src=${arr.meals[0].strMealThumb}>
-    <h3>${arr.meals[0].strMeal}</h3> </div>`;
-
-  randomMealCard.addEventListener("click", (e) => {
+  mealCard.innerHTML = `<div class="meal">
+    <img class="meals" src=${mealData.meals[0].strMealThumb}>
+    <h3>${mealData.meals[0].strMeal}</h3> </div>`;
+  mealCard.addEventListener("click", (e) => {
     const parent = e.target.parentNode;
     const h3 = parent.getElementsByTagName("h3")[0];
-
-    ingredientListMaker(h3.innerText);
+    createIngredientList(h3.innerText);
   });
 }
 
-searchButton.onclick = () => {
-  let input = searchBar.value;
 
-  if (selectedMode == undefined) {
-    window.alert("Please select any mode of search.");
-  } else if (input == "") {
-    searchInput.innerText = "Please enter a keyword to search";
-    results.innerHTML = "";
-  } else {
-    searchInput.innerText = `Search Results for "${input}"`;
-    searchMealBasedOnInput(input, selectedMode);
-  }
-};
-
-async function searchMealBasedOnInput(input, mode) {
-  if (mode == "categ1") {
-    await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${input}`)
+async function searchMeals(userInput, category) {
+  if (category == "categ1") {
+    await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${userInput}`)
       .then((data) => data.json())
       .then((e) => {
         if (e.meals != null) {
-          renderSearchResult(e);
+          displaySearchResults(e);
         } else {
-          results.innerHTML = "";
-          results.innerText = "Not found!";
+          resultOutput.innerHTML = "";
+          resultOutput.innerText = "Recipe Not found.";
         }
       });
   } else {
-    // Handle other modes similarly if needed
+    //console.log(category)
   }
 }
 
-function renderSearchResult(arrays) {
-  results.innerHTML = "";
-  arrays.meals.forEach((array) => {
-    results.innerHTML += `<div class="meal">
-      <img class="meals" src=${array.strMealThumb}>
-      <h3>${array.strMeal}</h3> </div>`;
-  });
 
-  mealCard = document.querySelectorAll(".meal");
-  eventListener();
-}
-
-function eventListener() {
-  for (let i = 0; i < mealCard.length; i++) {
-    const element = mealCard[i];
-    element.onclick = (e) => {
-      const parent = e.target.parentNode;
-      const h3 = parent.getElementsByTagName("h3")[0];
-
-      ingredientListMaker(h3.innerText);
-    };
-  }
-}
-
-async function ingredientListMaker(name) {
+async function createIngredientList(mealName) {
   let list;
-
-  await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
+  await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
     .then((data) => data.json())
     .then((e) => {
       list = e;
     });
-
-  ingredients.innerHTML = "";
+    // console.log(list)
+  ingredientList.innerHTML = "";
 
   for (let i = 1; i < 21; i++) {
     let ingredient = list.meals[0][`strIngredient${i}`];
-
     if (!ingredient) {
       break;
     }
-
-    ingredients.innerHTML +=` ${i}.${ingredient}<br>`;
+    ingredientList.innerHTML +=` ${i}.${ingredient}<br>`;
   }
-
   modal.style.display = "block";
 }
 
-for (let i = 0; i < mode.length; i++) {
-  mode[i].onclick = (e) => {
-    for (let j = 0; j < mode.length; j++) {
-      mode[j].style.color = "rgb(29, 26, 26)";
-      mode[j].style.backgroundColor = "white";
-    }
 
-    mode[i].style.color = "white";
-    mode[i].style.backgroundColor = "black";
-    selectedMode = e.target.id;
-  };
+searchBtn.onclick = () => {
+  let inputText = searchInput.value;
+  if (selectedCategory == undefined) {
+
+  } else if (inputText == "") {
+    userInput.innerText = "Enter items in searchbar";
+    resultOutput.innerHTML = "";
+  } else {
+    userInput.innerText = `Search Results for "${inputText}"`;
+    searchMeals(inputText, selectedCategory);
+  }
+  // console.log("wevufvuv")
+  // console.log(searchMeals)
+};
+
+
+function displaySearchResults(arrays) {
+  resultOutput.innerHTML = "";
+  arrays.meals.forEach((array) => {
+    resultOutput.innerHTML += `<div class="meal">
+      <img class="meals" src=${array.strMealThumb}>
+      <h3>${array.strMeal}</h3> </div>`;
+  });
+  mealCards = document.querySelectorAll(".meal");
+  setEventListeners();
+  // console.l
 }
 
-homeButton.onclick = () => {
+// dom maniulation in js to show categories when we search
+for (let i = 0; i < categories.length; i++) {
+  categories[i].onclick = (e) => {
+    for (let j = 0; j < categories.length; j++) {
+      categories[j].style.color = "rgb(29, 26, 26)";
+      categories[j].style.backgroundColor = "white";
+    }
+    categories[i].style.color = "white";
+    categories[i].style.backgroundColor = "black";
+    selectedCategory = e.target.id;
+  };
+  // console.log(selectedCategory)
+}
+
+
+function setEventListeners() {
+  for (let i = 0; i < mealCards.length; i++) {
+    const element = mealCards[i];
+    element.onclick = (e) => {
+      const parent = e.target.parentNode;
+      const h3 = parent.getElementsByTagName("h3")[0];
+      createIngredientList(h3.innerText);
+    };
+  }
+}
+
+
+exitSpan.onclick = function () {
+  modal.style.display = "none";
+};
+
+
+homeBtn.onclick = () => {
   window.location = "index.html";
 };
 
-span.onclick = function () {
-  modal.style.display = "none";
-};
 
 window.onclick = function (event) {
   if (event.target == modal) {
@@ -140,4 +142,5 @@ window.onclick = function (event) {
   }
 };
 
-window.addEventListener("load", randomMeal);
+
+window.addEventListener("load", fetchRandomMeal);
